130afb670d58fc31adc73a6b7c6d42f2d65b0c66
vanshikasangwan github <vanshikasangwan999@gmail.com>
2023-03-19 23:25:30 +0530

    Creating Java Connection

---
+++ AddInteger.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+  
+public class AddInteger {
+  
+    public static void main(String[] args)
+    {
+        int a = 62;
+        int b = 18;
+        // It will return the sum of two arguments.
+        System.out.println("The sum is =" + Integer.sum(a, b));
+    }
+}
